d37c6129bde7422aabfd820b816e4f3093557d5f
13035809409@163.com <cgh13145288>
2021-05-20 18:20:36 +0800

    laravel急救室论坛项目

---
+++ pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+
+    </el-menu>
+    <view class="hero">
+1231
+    </view>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        activeIndex: '1',
+        activeIndex2: '1'
+      };
+    },
+    methods: {
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+  }
+</script>
+<style>
+  .hero {
+      width:100%;
+      height:260px;
+      display:block;
+      margin-top: -1px;
+      background: #e4edf6;
+      text-align: center;
+      color: #667f99;
+  }
+</style>
